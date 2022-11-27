e1046a09031fa91faf22342dad4d56731821337b
Tristan Davis <Tristandavis888@gmail.com>
2022-11-27 03:06:53 -0800

    added edit and remove features to remidners

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -41,11 +41,28 @@ let remindersController = {
   },
 
   update: (req, res) => {
-    // implement this code
+    let reminderId = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderId;
+    });
+    searchResult.title = req.body.title;
+    searchResult.description = req.body.description;
+    searchResult.completed = req.body.completed;
+    for (let i in database.cindy.reminders) {
+      if (database.cindy.reminders[i].id == reminderId) {
+        database.cindy.reminders[i] = searchResult;
+      }
+    }
+    res.redirect("/reminders");
   },
 
   delete: (req, res) => {
-    // Implement this code
+    let reminderId = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderId;
+    });
+    database.cindy.reminders.pop(searchResult);
+    res.redirect("/reminders");
   },
 };
 
